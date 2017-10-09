659d275cf1e159c0be8017184cf51d034c3b66da
fourlines <fourlines@foxmail.com>
2017-10-10 00:37:57 +0800

    create

---
+++ app/src/main/java/com/fourlines/fourlinesmusicplayer/player.java
@@ -0,0 +1,17 @@
+package com.fourlines.fourlinesmusicplayer;
+
+import android.content.Context;
+
+/**
+ * 简介：
+ * 作者：李自恒   QQ:258794012
+ * 时间： 2017/10/9.
+ */
+
+public class player {
+    private  static Context mContext;
+
+    private player(Context context) {
+        mContext = context;
+    }
+}
